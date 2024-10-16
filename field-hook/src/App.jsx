ede3f27c02a72164d10f8b190432b55e65999d89
Stephen <hellostephenwm@gmail.com>
2024-10-16 15:44:58 -0400

    feat: employed the useField hook

--- field-hook/src/App.jsx
+++ field-hook/src/App.jsx
@@ -1,25 +1,40 @@
 import React from "react";
+import useField from "./hooks/useField";
 
 const App = () => {
-	const name = "";
-	const birthdate = "";
-	const height = "";
+	const name = useField("text");
+	const birthdate = useField("date");
+	const height = useField("number");
 
 	return (
 		<div>
 			<form>
 				name:
-				<input type="text" />
+				<input
+					type="text"
+					value={name.value}
+					onChange={name.onChange}
+				/>
 				<br />
 				birth date:
-				<input type="date" />
+				<input
+					type="date"
+					value={birthdate.value}
+					onChange={birthdate.onChange}
+				/>
 				<br />
 				height:
-				<input type="number" />
+				<input
+					type="number"
+					value={height.value}
+					onChange={height.onChange}
+				/>
 			</form>
 
-			<div>
-				{name} {birthdate} {height}
+			<div style={{ marginTop: "20px" }}>
+				name: {name.value} <br />
+				birthdate: {birthdate.value} <br />
+				height: {height.value} ft
 			</div>
 		</div>
 	);
