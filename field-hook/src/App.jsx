395ceae806aacc1e6f294c34cce07947af8669e1
Stephen <hellostephenwm@gmail.com>
2024-10-16 15:37:17 -0400

    chore: scaffold app UI

--- field-hook/src/App.jsx
+++ field-hook/src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const App = () => {
-	return <div>App</div>;
+	const name = "";
+	const birthdate = "";
+	const height = "";
+
+	return (
+		<div>
+			<form>
+				name:
+				<input type="text" />
+				<br />
+				birth date:
+				<input type="date" />
+				<br />
+				height:
+				<input type="number" />
+			</form>
+
+			<div>
+				{name} {birthdate} {height}
+			</div>
+		</div>
+	);
 };
 
 export default App;
