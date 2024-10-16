08dd157f77f1e122d3d37015516a770fead95183
Stephen <hellostephenwm@gmail.com>
2024-10-16 15:11:43 -0400

    chore: scaffold counter UI

--- counter-hook/src/App.jsx
+++ counter-hook/src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div>
+			<div>0</div>
+			<button onClick={() => console.log("plus")}>plus</button>
+			<button onClick={() => console.log("minus")}>minus</button>
+			<button onClick={() => console.log("zero")}>zero</button>
+		</div>
+	);
 };
 
 export default App;
